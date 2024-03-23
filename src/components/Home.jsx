153eb00ababf718418f879c50ad0265b914fc8cc
sandali24 <sandalisiri58@gmail.com>
2024-03-23 23:25:20 +0530

    Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.jpg";
+import { MdKeyboardArrowRight } from "react-icons/md";
+// import { Link } from "react-scroll";
+
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="w-full h-screen bg-gradient-to-b from-black via-black to-blue-800"
+    >
+      <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I have 8 years of experience building and desgining software.
+            Currently, I love to work on web application using technologies like
+            React, Tailwind, Next JS and GraphQL.
+          </p>
+          <div>
+<button className='text-white w-fit px-6 py-3 my-2 
+flex items-center rounded-md bg-gradient-to-r from-cyan-500
+ to-blue-500 cursor-pointer'> Portfolio
+    <span className='hover:rotate-90 duration-300'>
+<MdKeyboardArrowRight />
+    </span>
+ </button>
+
+
+
+
+
+            {/* <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-1" />
+              </span>
+            </Link> */}
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full md:h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
+
+// 49:00
