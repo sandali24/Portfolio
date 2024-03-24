c084c8c072e4755736753849fc13575e9da84749
sandali24 <sandalisiri58@gmail.com>
2024-03-24 12:17:41 +0530

    Done

---
+++ src/components/About.jsx
@@ -0,0 +1,75 @@
+// import React from "react";
+
+// const About = () => {
+//   return (
+//     <div
+//       name="about"
+//       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+//     >
+//       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+//         <div className="pb-8">
+//           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+//             About
+//           </p>
+//         </div>
+//         <p className="text-xl mt-20">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+//           qui fugit numquam! Cum vitae temporibus molestiae dicta illum
+//           laboriosam similique at mollitia nihil iusto repellat numquam eos illo
+//           perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
+//           maiores. Voluptatum quisquam dignissimos tempore asperiores sit
+//           ratione officiis officia alias vitae.
+//         </p>
+
+//         <br />
+
+//         <p className="text-xl">
+//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+//           qui fugit numquam! Cum vitae temporibus molestiae dicta illum
+//           laboriosam similique at mollitia nihil iusto repellat numquam eos illo
+//           perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
+//           maiores. Voluptatum quisquam dignissimos tempore asperiores sit
+//           ratione officiis officia alias vitae.
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
+
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About Me
+          </p>
+        </div>
+        <p className="text--5xl mt-20">
+        As a student who pursued the mathematics stream during my A/Ls, my journey into the world of software engineering began after completing my exams. Eager to expand my knowledge and skills in the software field, 
+        I actively sought opportunities to learn at ICET. These resources provided me with a solid foundation and helped me grasp the fundamentals of software engineering. 
+        Through self-study and practice, I developed a deep interest in coding and problem-solving, which fueled my determination to pursue a career in software engineering. </p> <br />
+
+        <p>
+
+Motivated by my passion for software engineering, I made the decision to join the Informatics Institute of Technology (IIT) to pursue a degree in the field. This step marked a significant turning point in my journey. At IIT, I have been exposed to a rigorous curriculum that covers a wide range of topics related to software engineering. From programming languages to software development methodologies, I have had the opportunity to gain practical knowledge and hands-on experience through projects and workshops. The supportive environment at IIT has allowed me to grow both personally and professionally, nurturing my skills and fostering a deep understanding of
+ the industry. </p> <br />
+ <p>
+
+Looking ahead, my future plans in my software engineering career are ambitious and exciting. I aspire to become a skilled and versatile software engineer, capable of creating innovative solutions and contributing to the technological advancements of our society. To achieve this, I plan to actively engage in internships and practical projects to gain industry experience. Additionally, I aim to stay updated with the latest developments in the field by attending conferences, participating in workshops, and continuously expanding my knowledge through self-learning. My ultimate goal is to make a positive impact through my work, utilizing my skills to solve real-world problems and contribute to the ever-evolving software engineering landscape.
+        </p>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,91 @@
-import React from 'react'
-import HeroImage from "../assets/heroImage.jpg";
-import { MdKeyboardArrowRight } from "react-icons/md";
-// import { Link } from "react-scroll";
+// import React from 'react'
+// import HeroImage from "../assets/heroImage.jpg";
+// import { MdKeyboardArrowRight } from "react-icons/md";
+// // import { Link } from "react-scroll";
 
+
+// const Home = () => {
+//   return (
+//     <div
+//       name="home"
+//       className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
+//     >
+//       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
+//         <div className="flex flex-col justify-center h-full">
+//           <h2 className="text-4xl sm:text-7xl font-bold text-white">
+//             I'm a Full Stack Developer
+//           </h2>
+//           <p className="text-gray-500 py-4 max-w-md">
+//             I have 8 years of experience building and desgining software.
+//             Currently, I love to work on web application using technologies like
+//             React, Tailwind, Next JS and GraphQL.
+//           </p>
+//           <div>
+// <button className='group text-white w-fit px-6 py-3 my-2 
+// flex items-center rounded-md bg-gradient-to-r from-gray-500
+//  to-black-500 cursor-pointer'> Portfolio
+//     <span className='group-hover:rotate-90 duration-300'>
+// <MdKeyboardArrowRight size={25} className='ml-'/>
+//     </span>
+//  </button>
+
+
+
+
+
+//             {/* <Link
+//               to="portfolio"
+//               smooth
+//               duration={500}
+//               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+//             >
+//               Portfolio
+//               <span className="group-hover:rotate-90 duration-300">
+//                 <HiArrowNarrowRight className="ml-1" />
+//               </span>
+//             </Link> */}
+//           </div>
+//         </div>
+//         <div>
+//           <img
+//             src={HeroImage}
+//             alt="my profile"
+//             className="rounded-2xl mx-auto w-2/3 md:w-full md:h-auto"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+// // 49:00
+
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import HeroImage from "../assets/heroImage.jpg";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
     <div
       name="home"
-      className="w-full h-screen bg-gradient-to-b from-black via-black to-blue-800"
+      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
     >
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+          <h2 className="text-4xl sm:text-5xl font-bold text-white">
+            I'm Sandali <br></br>
+            SE Undergraduate
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+          I am an enthusiastic undergraduate student 
+          pursuing a degree in Software Engineering . 
+          With a passion for coding and problem-solving, I am constantly expanding my knowledge and skills in this exciting field.
           </p>
           <div>
-<button className='text-white w-fit px-6 py-3 my-2 
-flex items-center rounded-md bg-gradient-to-r from-cyan-500
- to-blue-500 cursor-pointer'> Portfolio
-    <span className='hover:rotate-90 duration-300'>
-<MdKeyboardArrowRight />
-    </span>
- </button>
-
-
-
-
-
-            {/* <Link
+            <Link
               to="portfolio"
               smooth
               duration={500}
@@ -43,21 +95,19 @@ flex items-center rounded-md bg-gradient-to-r from-cyan-500
               <span className="group-hover:rotate-90 duration-300">
                 <HiArrowNarrowRight className="ml-1" />
               </span>
-            </Link> */}
+            </Link>
           </div>
         </div>
         <div>
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full md:h-auto"
+            className="rounded-2xl mx-auto w-2/3 "
           />
-        </div>
+        </div> i want this image to be round in size and small
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-// 49:00
+export default Home;
